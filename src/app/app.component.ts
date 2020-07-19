f4156e3488bf6e79b9d65f3ab416ebd40ab86097
syamiiitn <syamiiitn@gamil.com>
2020-07-19 09:43:58 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'angprotest';
+
+  isEven:boolean;
+  date:any;
+  time=setInterval(()=>{
+    let hr =new Date().getHours();
+     let min =new Date().getMinutes();
+     let sec=new Date().getSeconds();
+     if(sec%2==0){
+       this.isEven=true;
+       this.date=hr+":"+min+":"+sec;
+     }else{
+       this.isEven=false;
+      this.date=hr+":"+min+":"+sec;
+     }
+   },300);
+  ngDoCheck(){
+    
+  }
+
+
+
+
+
 }
